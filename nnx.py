f1106226446625c4acd662523ef3d02110eaf09a
Paseville <emueller.de@gmail.com>
2022-03-31 12:36:32 +0200

    fix totalBill sum

--- nnx.py
+++ nnx.py
@@ -9,15 +9,18 @@ randomitems = ["Bayreuther", "Weizen", "Limo", "Cola", "Sprite", "Bionade", "Des
 #generate 10 bills
 for x in range(10):
 	arrayItems = []
+	totalBill = 0
 	#for each bill generate random amount of items bought
 	for x in range(random.randint(1,9)):
 		priceOne = random.randint(1,10)
 		itemsBought = random.randint(0,10)
+		priceAll = priceOne * itemsBought
+		totalBill += priceAll
 		oneItem = {
 			"itemName": randomitems[random.randint(0,14)],
 			"itemPriceOne": priceOne,
 			"itemsBought" : itemsBought,
-			"itemPriceAll": priceOne * itemsBought
+			"itemPriceAll": priceAll
 			}
 		arrayItems.append(oneItem)
 	answers = {
@@ -25,7 +28,7 @@ for x in range(10):
 		"randomAuthKey": str(round(time.time()* 100000)),
 		"done": 0,
 		"boughtItems": arrayItems,
-		"totalBill": 50,
+		"totalBill": totalBill,
 	}
 	headers = {'content-type': 'application/json', 'accept': 'text/plain'}
 	r = requests.post("https://billgatesprojekt.herokuapp.com/create-new?auth=1234",headers=headers, data=json.dumps(answers), )
